e948f48deb5af249d73b33d938e80bbc468c327b
VICTOR-PETRU MOLDOVAN <victor.moldovan@stud.ubbcluj.ro>
2023-11-27 18:12:48 +0200

    Laborator8 - Complet

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace Moldovan_Victor_Petru_Lab7
+﻿using System;
+using Moldovan_Victor_Petru_Lab7.Data;
+using System.IO;
+
+namespace Moldovan_Victor_Petru_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
