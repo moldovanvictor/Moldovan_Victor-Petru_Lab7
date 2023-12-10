2758bf08dac64f1230122795a854300a29d6e0c0
VICTOR-PETRU MOLDOVAN <victor.moldovan@stud.ubbcluj.ro>
2023-12-10 17:54:47 +0200

    Lab9+Tema Done

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -21,5 +21,33 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        Product p;
+        if (listView.SelectedItem != null)
+        {
+            p = listView.SelectedItem as Product;
+            await App.Database.DeleteProductFromListAsync((ShopList)BindingContext, p.ID );
+            await Navigation.PopAsync();
+
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
 
 }
